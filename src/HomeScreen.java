e8b3783b036f158a7959eddaf0dfeb39948f48af
liamaccenture <liam.r.grimwood@accenture.com>
2018-10-25 11:39:37 +0100

    First Commit

---
+++ src/HomeScreen.java
@@ -0,0 +1,28 @@
+import javax.swing.JFrame;
+
+public class HomeScreen extends JFrame{
+
+	//Variables
+	
+	
+	
+	//Constructor
+		
+	public HomeScreen() {
+	
+		this.setTitle("Asset Search - Home Page");
+		this.setBounds(100,100,1000,600);
+		IniComp();
+		this.setVisible(true);
+	
+	}
+	
+	//Methods
+	
+	public void IniComp() {
+		
+		
+		
+	}
+
+}
